3e0bf7130b15050df8b9d3f69b34f9e2f6309882
Glenfierresijabat <pernandosijabat5@gmail.com>
2024-11-04 19:28:21 +0700

    uts1

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from "react";
+import SignInPage from "./pages/signIn";
+import SignUpPage from "./pages/signUp";
+import ForgotPassword from "./components/Fragments/ForgotPassword";
+import ErrorRoute from "./pages/errorRoute";
+import DashboardPage from "./pages/dashboard";
+import BalancePage from "./pages/balance";
+import ExpensesPage from "./pages/ExpensesPage";
+
+const App = () => {
+  const myRouter = createBrowserRouter([
+	{
+	  path: "/",
+      element: <DashboardPage/>,
+	  errorElement: <ErrorRoute/>,
+
+	},
+	{
+		path: "/login",
+		element: <SignInPage/>,
+	},
+	{
+		path: "/register",
+		element: <SignUpPage/>,
+	},
+	{
+		path: "/forgot-password",
+		element: <ForgotPassword /> ,
+	},
+	{
+		path: "/balance",
+		element: <BalancePage/>,
+  
+	  },
+	    {
+      path: "/expenses",
+      element: <ExpensesPage />,
+    },
+
+	]);
+
+  return (
+    <>
+      <RouterProvider router={myRouter} />
+    </>
+  );
+};
+
+export default App;
